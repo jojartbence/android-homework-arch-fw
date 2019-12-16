9d9635ecafe98f8c3ed3b19f24cca56e7d1625d9
jojartbence <jojart.bence96@gmail.com>
2019-12-16 22:47:45 +0100

    Automatically changing to login fragment after 1 sec

--- app/src/main/java/com/jojartbence/archeologicalfieldwork/OpeningScreenFragment.kt
+++ app/src/main/java/com/jojartbence/archeologicalfieldwork/OpeningScreenFragment.kt
@@ -2,15 +2,20 @@ package com.jojartbence.archeologicalfieldwork
 
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
  */
 class OpeningScreenFragment : Fragment() {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,4 +25,21 @@ class OpeningScreenFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_opening_screen, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        val timer = object: CountDownTimer(1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {}
+
+            override fun onFinish() {
+                changeToLoginFragment()
+            }
+        }
+        timer.start()
+    }
+
+    fun changeToLoginFragment() {
+        navController.navigate(R.id.action_openingScreenFragment_to_loginFragment)
+    }
 }
