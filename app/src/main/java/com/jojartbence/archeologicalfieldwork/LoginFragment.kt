29a3ccf139278848c4153ea52fbb98d213d64f52
jojartbence <jojart.bence96@gmail.com>
2019-12-17 21:42:04 +0100

    Firebase and ViewModel dependency added. LoginViewModel empty class added

--- app/src/main/java/com/jojartbence/archeologicalfieldwork/LoginFragment.kt
+++ app/src/main/java/com/jojartbence/archeologicalfieldwork/LoginFragment.kt
@@ -6,16 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
 
 /**
  * A simple [Fragment] subclass.
  */
 class LoginFragment : Fragment() {
 
+    private lateinit var viewModel: ViewModel
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        viewModel = ViewModelProviders.of(this)[LoginViewModel::class.java]
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
