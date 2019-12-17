4507a4771f65ed3870af536591364efb5ba3ed3c
jojartbence <jojart.bence96@gmail.com>
2019-12-17 22:33:31 +0100

    Able to sign up and log in via firebase auth

--- app/src/main/java/com/jojartbence/archeologicalfieldwork/LoginFragment.kt
+++ app/src/main/java/com/jojartbence/archeologicalfieldwork/LoginFragment.kt
@@ -6,26 +6,82 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModel
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_login.*
+
 
 /**
  * A simple [Fragment] subclass.
  */
 class LoginFragment : Fragment() {
 
-    private lateinit var viewModel: ViewModel
+    private val viewModel by lazy { ViewModelProviders.of(this)[LoginViewModel::class.java] }
+
+    lateinit var navController: NavController
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-        viewModel = ViewModelProviders.of(this)[LoginViewModel::class.java]
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+
+        logIn.setOnClickListener {
+            logIn()
+        }
+
+        signUp.setOnClickListener {
+            signUp()
+        }
+    }
+
+
+    fun logIn() {
+        val email = email.text.toString()
+        val password = password.text.toString()
+        if (email == "" || password == "") {
+            Toast.makeText(activity, "Please provide email + password", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            viewModel.doLogin(email,password,
+                doOnSuccess = {
+                    navController.navigate(R.id.action_loginFragment_to_siteListFragment)
+                },
+                doOnFailure = {},
+                doOnComplete = {})
+        }
+    }
+
+
+    fun signUp() {
+        val email = email.text.toString()
+        val password = password.text.toString()
+        if (email == "" || password == "") {
+            Toast.makeText(activity, "Please provide email + password", Toast.LENGTH_SHORT).show()
+        }
+        else {
+            viewModel.doSignUp(email,password,
+                doOnSuccess = {
+                    navController.navigate(R.id.action_loginFragment_to_siteListFragment)
+                },
+                doOnFailure = {},
+                doOnComplete = {})
+        }
+    }
+
+
+
 
 
 }
--- app/src/main/java/com/jojartbence/archeologicalfieldwork/LoginViewModel.kt
+++ app/src/main/java/com/jojartbence/archeologicalfieldwork/LoginViewModel.kt
@@ -1,7 +1,36 @@
 package com.jojartbence.archeologicalfieldwork
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginViewModel: ViewModel() {
+
+    var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+
+    fun doLogin(email: String, password: String,
+                doOnSuccess: () -> Unit, doOnFailure: (Exception) -> Unit, doOnComplete: () -> Unit) {
+
+        auth.signInWithEmailAndPassword(email, password).addOnSuccessListener {
+            doOnSuccess()
+        }.addOnFailureListener {
+            doOnFailure(it)
+        }.addOnCompleteListener {
+            doOnComplete()
+        }
+    }
+
+
+    fun doSignUp(email: String, password: String,
+                 doOnSuccess: () -> Unit, doOnFailure: (Exception) -> Unit, doOnComplete: () -> Unit) {
+
+        auth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+            doOnSuccess()
+        }.addOnFailureListener {
+            doOnFailure(it)
+        }.addOnCompleteListener {
+            doOnComplete()
+        }
+    }
 
 }
