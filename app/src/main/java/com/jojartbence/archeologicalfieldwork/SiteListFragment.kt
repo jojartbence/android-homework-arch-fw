18bded28f52ac2e6371c44f46af8002b06067d18
jojartbence <jojart.bence96@gmail.com>
2019-12-18 11:52:44 +0100

    SiteListViewModel added

--- app/src/main/java/com/jojartbence/archeologicalfieldwork/SiteListFragment.kt
+++ app/src/main/java/com/jojartbence/archeologicalfieldwork/SiteListFragment.kt
@@ -6,17 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
 
 /**
  * A simple [Fragment] subclass.
  */
 class SiteListFragment : Fragment() {
 
+    private val viewModel by lazy { ViewModelProviders.of(this)[SiteListViewModel::class.java] }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        viewModel.createDatabase(activity!!.applicationContext)
+
         return inflater.inflate(R.layout.fragment_site_list, container, false)
     }
 
