912d82cfc081763858f7f6238a94c6f248f23ba8
jojartbence <jojart.bence96@gmail.com>
2019-12-22 22:02:19 +0100

    Unnecessary import removed

--- app/src/main/java/com/jojartbence/archeologicalfieldwork/SiteViewModel.kt
+++ app/src/main/java/com/jojartbence/archeologicalfieldwork/SiteViewModel.kt
@@ -1,6 +1,5 @@
 package com.jojartbence.archeologicalfieldwork
 
-import android.app.Activity
 import android.content.Intent
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
