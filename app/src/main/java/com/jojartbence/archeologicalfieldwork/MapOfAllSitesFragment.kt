8826918fda96cd9f5e37e89f3c20f8005b63b2ba
jojartbence <jojart.bence96@gmail.com>
2019-12-29 21:38:31 +0100

    Fragment layout done

--- app/src/main/java/com/jojartbence/archeologicalfieldwork/MapOfAllSitesFragment.kt
+++ app/src/main/java/com/jojartbence/archeologicalfieldwork/MapOfAllSitesFragment.kt
@@ -6,15 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 
 class MapOfAllSitesFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MapOfAllSitesFragment()
-    }
+    private val viewModel by lazy { ViewModelProviders.of(this)[MapOfAllSitesViewModel::class.java] }
 
-    private lateinit var viewModel: MapOfAllSitesViewModel
+    lateinit var navController: NavController
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +24,10 @@ class MapOfAllSitesFragment : Fragment() {
         return inflater.inflate(R.layout.map_of_all_sites_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MapOfAllSitesViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
     }
 
 }
