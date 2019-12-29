5c89c5e612efffcc6158bcdafc78a512909ce240
jojartbence <jojart.bence96@gmail.com>
2019-12-29 22:22:20 +0100

    Map of all sites basically works, can select sites, title and description showed.

--- app/src/main/java/com/jojartbence/archeologicalfieldwork/MapOfAllSitesFragment.kt
+++ app/src/main/java/com/jojartbence/archeologicalfieldwork/MapOfAllSitesFragment.kt
@@ -6,11 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.jojartbence.model.SiteModel
+import kotlinx.android.synthetic.main.fragment_site.*
+import kotlinx.android.synthetic.main.fragment_site.siteTitle
+import kotlinx.android.synthetic.main.map_of_all_sites_fragment.*
 
 
-class MapOfAllSitesFragment : Fragment() {
+class MapOfAllSitesFragment : Fragment(), GoogleMap.OnMarkerClickListener {
 
     private val viewModel by lazy { ViewModelProviders.of(this)[MapOfAllSitesViewModel::class.java] }
 
@@ -28,6 +38,86 @@ class MapOfAllSitesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         navController = Navigation.findNavController(view)
+
+
+        val selectedSiteObserver = Observer<SiteModel> {
+            showSelectedSite()
+        }
+        viewModel.selectedSite.observe(this, selectedSiteObserver)
+
+
+        allSitesMapView.onCreate(savedInstanceState)
+        createMap()
     }
+
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+        viewModel.selectSiteById(marker.tag as Long)
+        return true
+    }
+
+
+    private fun createMap() {
+        allSitesMapView.getMapAsync {
+            val map = it
+            map.setOnMarkerClickListener(this)
+            map.uiSettings.isZoomControlsEnabled = true
+            viewModel.getAllSites().forEach {
+                val loc = LatLng(it.location.lat, it.location.lng)
+                val options = MarkerOptions().title(it.title).position(loc)
+                map.addMarker(options).tag = it.id
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.location.zoom))
+            }
+        }
+    }
+
+
+    private fun showSelectedSite() {
+        selectedSiteTitle.text = viewModel.selectedSite.value?.title
+        selectedSiteDescription.text = viewModel.selectedSite.value?.description
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        allSitesMapView.onDestroy()
+    }
+
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        allSitesMapView.onLowMemory()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        allSitesMapView.onPause()
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        allSitesMapView.onResume()
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        allSitesMapView.onSaveInstanceState(outState)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        allSitesMapView.onStart()
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        allSitesMapView.onStop()
+    }
+
 
 }
--- app/src/main/java/com/jojartbence/archeologicalfieldwork/MapOfAllSitesViewModel.kt
+++ app/src/main/java/com/jojartbence/archeologicalfieldwork/MapOfAllSitesViewModel.kt
@@ -1,7 +1,22 @@
 package com.jojartbence.archeologicalfieldwork
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jojartbence.model.SiteModel
+import com.jojartbence.model.SiteRepository
 
 class MapOfAllSitesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val selectedSite = MutableLiveData<SiteModel>()
+
+
+    fun getAllSites(): List<SiteModel> {
+        return SiteRepository.findAll()
+    }
+
+
+    fun selectSiteById(id: Long) {
+        selectedSite.value = SiteRepository.findById(id)
+    }
+
 }
