2dfd58a112cf3a5e76b2789a21639b33cc1c64bd
jojartbence <jojart.bence96@gmail.com>
2019-12-30 21:52:48 +0100

    Deleted unneccesary LocationHelper functions

--- app/src/main/java/com/jojartbence/helpers/LocationHelpers.kt
+++ app/src/main/java/com/jojartbence/helpers/LocationHelpers.kt
@@ -1,12 +1,9 @@
 package com.jojartbence.helpers
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.pm.PackageManager
-import android.util.Log
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.LocationRequest
 
 const val REQUEST_PERMISSIONS_REQUEST_CODE = 34
 
@@ -18,29 +15,4 @@ fun checkLocationPermissions(activity: Activity) : Boolean {
         ActivityCompat.requestPermissions(activity, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSIONS_REQUEST_CODE)
         return false
     }
-}
-
-fun isPermissionGranted(code: Int, grantResults: IntArray): Boolean {
-    var permissionGranted = false;
-    if (code == REQUEST_PERMISSIONS_REQUEST_CODE) {
-        when {
-            grantResults.isEmpty() -> Log.i("Location", "User interaction was cancelled.")
-            (grantResults[0] == PackageManager.PERMISSION_GRANTED) -> {
-                permissionGranted = true
-                Log.i("Location", "Permission Granted.")
-            }
-            else -> Log.i("Location", "Permission Denied.")
-        }
-    }
-    return permissionGranted
-}
-
-@SuppressLint("RestrictedApi")
-fun createDefaultLocationRequest() : LocationRequest {
-    val locationRequest = LocationRequest().apply {
-        interval = 10000
-        fastestInterval = 5000
-        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-    }
-    return locationRequest
 }
