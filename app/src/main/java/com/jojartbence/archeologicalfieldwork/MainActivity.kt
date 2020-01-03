eb5e671c3f4ba0d3828168a52c0e5de8c5fde9f5
jojartbence <jojart.bence96@gmail.com>
2020-01-03 19:32:08 +0100

    Navigation drawer works.

--- app/src/main/java/com/jojartbence/archeologicalfieldwork/MainActivity.kt
+++ app/src/main/java/com/jojartbence/archeologicalfieldwork/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
@@ -21,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         val topLevelDestinations = setOf(R.id.nav_addSite, R.id.nav_mapOfAllSites, R.id.nav_favouriteSites, R.id.nav_settings, R.id.nav_siteList, R.id.nav_logout)
-        appBarConfig = AppBarConfiguration.Builder(topLevelDestinations).build()
+        appBarConfig = AppBarConfiguration.Builder(topLevelDestinations).setDrawerLayout(drawer_layout).build()
 
 
 
@@ -32,7 +33,7 @@ class MainActivity : AppCompatActivity() {
 
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfig) || super.onSupportNavigateUp()
     }
 
 
