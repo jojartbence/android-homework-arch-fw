2a5d32169e80a3fbf6205458dfe454e3f93855d9
jojartbence <jojart.bence96@gmail.com>
2020-01-11 22:48:22 +0100

    A backup database introduced and can be used.

--- app/src/main/java/com/jojartbence/model/SiteRepository.kt
+++ app/src/main/java/com/jojartbence/model/SiteRepository.kt
@@ -5,7 +5,7 @@ import android.content.Context
 
 object SiteRepository {
     private lateinit var siteStore: SiteStoreInterface
-    private var backupSiteStore: SiteBackupStoreInterface? = null
+    private var backupSiteStore: SiteStoreInterface? = null
 
 
     fun createDatabase(context: Context, userEmail: String) {
@@ -14,8 +14,16 @@ object SiteRepository {
     }
 
 
+    fun createDatabaseUsingBackup(context: Context, userEmail: String) {
+        siteStore = SiteJsonStore(context, userEmail)
+    }
+
+
     fun fetchSites(onSitesReady: () -> Unit) {
-        siteStore.fetchSites(onSitesReady)
+        siteStore.fetchSites {
+            onSitesReady()
+            backupSiteStore?.initAsBackupStore(siteStore)
+        }
     }
 
 
@@ -26,16 +34,19 @@ object SiteRepository {
 
     fun create(site: SiteModel) {
         siteStore.create(site)
+        backupSiteStore?.create(site)
     }
 
 
     fun update(site: SiteModel) {
         siteStore.update(site)
+        backupSiteStore?.update(site)
     }
 
 
     fun delete(site: SiteModel) {
         siteStore.delete(site)
+        backupSiteStore?.delete(site)
     }
 
 
@@ -46,12 +57,6 @@ object SiteRepository {
 
     fun clear() {
         siteStore.clear()
-    }
-
-
-    private fun initBackupStore() {
-        siteStore.findAll().forEach {
-            if (backupSiteStore.findById(it.id) != null)
-        }
+        backupSiteStore?.clear()
     }
 }
