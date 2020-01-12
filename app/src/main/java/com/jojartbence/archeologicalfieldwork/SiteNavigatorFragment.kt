7ba2369dac29aeea361c9012382836dd792cf7d5
jojartbence <jojart.bence96@gmail.com>
2020-01-12 12:30:36 +0100

    Navigator fragment opens google map, and shows site location as a marker

--- app/src/main/java/com/jojartbence/archeologicalfieldwork/SiteNavigatorFragment.kt
+++ app/src/main/java/com/jojartbence/archeologicalfieldwork/SiteNavigatorFragment.kt
@@ -6,15 +6,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_site.*
+import kotlinx.android.synthetic.main.fragment_site.mapView
+import kotlinx.android.synthetic.main.site_edit_location_fragment.*
 
 
 class SiteNavigatorFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = SiteNavigatorFragment()
+    private val viewModel by lazy { ViewModelProviders.of(this)[SiteNavigatorViewModel::class.java] }
+
+    lateinit var navController: NavController
+
+    var googleMap: GoogleMap? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        viewModel.attachLocation(arguments?.getParcelable("location"))
+
+        super.onCreate(savedInstanceState)
     }
 
-    private lateinit var viewModel: SiteNavigatorViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +39,69 @@ class SiteNavigatorFragment : Fragment() {
         return inflater.inflate(R.layout.site_navigator_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(SiteNavigatorViewModel::class.java)
-        // TODO: Use the ViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = Navigation.findNavController(view)
+
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync {
+            it?.uiSettings?.isZoomControlsEnabled = true
+            val options = MarkerOptions()
+                .title("Placemark")
+                .snippet("GPS : " + viewModel.getSiteLatLng().toString())
+                .draggable(false)
+                .position(viewModel.getSiteLatLng())
+            it.addMarker(options)
+            it.moveCamera(CameraUpdateFactory.newLatLngZoom(viewModel.getSiteLatLng(), viewModel.siteLocation.zoom))
+            lat.text = "Lat: %.6f".format(viewModel.siteLocation.lat)
+            lng.text = "Lng: %.6f".format(viewModel.siteLocation.lng)
+        }
     }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView.onDestroy()
+    }
+
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+
 
 }
--- app/src/main/java/com/jojartbence/archeologicalfieldwork/SiteNavigatorViewModel.kt
+++ app/src/main/java/com/jojartbence/archeologicalfieldwork/SiteNavigatorViewModel.kt
@@ -1,7 +1,21 @@
 package com.jojartbence.archeologicalfieldwork
 
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.model.LatLng
+import com.jojartbence.model.Location
 
 class SiteNavigatorViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    lateinit var siteLocation: Location
+
+
+    fun attachLocation(location: Location?) {
+        this.siteLocation = location ?: Location()
+    }
+
+
+    fun getSiteLatLng(): LatLng {
+        return LatLng(siteLocation.lat, siteLocation.lng)
+    }
+
 }
